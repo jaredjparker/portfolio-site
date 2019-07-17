2dc8322f534678618a2fc2f80d78974c733e4fe7
VIISilver <jparke71@hotmail.com>
2019-07-16 23:27:50 -0600

    Minor styling adjustments and added images.

--- src/components/jump/Jump.js
+++ src/components/jump/Jump.js
@@ -4,8 +4,8 @@ import './Jump.css'
 export default class Jump extends Component {
     render() {
         return (
-            <div className='jump-menu-wrapper'>
-                <div className='jump-menu-item'>
+            <div className='jump-menu-wrapper border'>
+                <div className='jump-menu-item border'>
                     <a>Home</a>
                     <a>About</a>
                     <a>Projects</a>
