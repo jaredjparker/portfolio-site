9b4a17fd1e4cf6341798a0d857fc301313691acf
VIISilver <jparke71@hotmail.com>
2019-07-17 23:42:47 -0600

    Fixed the position of the jump menu.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ClearlyMyName from './ClearlyMyName'
 import Resume from './Resume'
+import './Home.css'
 
 export default class Home extends Component {
     render() {
