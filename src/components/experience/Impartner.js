3c545d43d0b53cf80af8171516edb722c165af8e
VIISilver <jparke71@hotmail.com>
2019-08-05 18:50:16 -0600

    Content and styling for Contact has been completed.

--- src/components/experience/Impartner.js
+++ src/components/experience/Impartner.js
@@ -4,9 +4,9 @@ import './Impartner.css'
 export default class Impartner extends Component {
     render() {
         return (
-            <div className='experience-basic'>
-                <h2>Customer Support Specialist</h2>
-                <p>HTML/CSS Contractor</p>
+            <div className='experience-basic offset-to-left '>
+                <h2>Impartner Software</h2>
+                <p>Customer Support Specialist</p>
                 <p>July 2018 - Current</p>
                 <ul>
                     <li>Receive requests from customers regarding front-end changes that they want completed on their portal. After responding to the requests I complete the items detailed and continue working with the customer until all work is completed and pushed to the Production environment. I spend most of my day working with HTML, CSS, and Impartner's Proprietary scripting language. I occasionally use Javascript and C# to complete customer requests. Perforce is the versioning software that is used at Impartner.</li>
