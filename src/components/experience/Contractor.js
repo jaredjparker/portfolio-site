85d1eae4e8bf29190ea5968aabccf2ee1b8c31cc
VIISilver <jparke71@hotmail.com>
2019-08-18 17:08:25 -0600

    Text edits completed on one of the projects.

--- src/components/experience/Contractor.js
+++ src/components/experience/Contractor.js
@@ -9,7 +9,7 @@ export default class Contractor extends Component {
                 <p>HTML/CSS Contractor</p>
                 <p>February - July 2018</p>
                 <ul>
-                    <li>Assisting in the transition of a revamp of several portals for a major customer. The majority of the work is transferring the old HTML elements into the new code base, making proper styling adjustments, and interpreting any C# (found on pages that I am working on) into Impartner's proprietary scripting language called Momentum. Using Perforce for version control.</li>
+                    <li>Assisting in the transition of a rebrand of several portals for a major customer. The majority of the work is transferring the old HTML elements into the new code base, making proper styling adjustments, and interpreting any C# code into Momentum, which is Impartner's proprietary scripting language. Using Perforce for version control.</li>
                 </ul>
             </div>
         )
