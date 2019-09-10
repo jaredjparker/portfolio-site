dce13434050ea87b60097883d8cef1fb667c7d24
VIISilver <jparke71@hotmail.com>
2019-09-10 17:56:02 -0600

    Slider Btns are functional components with basic styling.

--- src/components/projects/sliderComps/ProjectSlider.js
+++ src/components/projects/sliderComps/ProjectSlider.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
 import Health from '../Health'
+import SliderBtn from './SliderBtn'
 import './ProjectSlider.css'
 
 export default class ProjectSlider extends Component {
     render() {
         return (
             <div className='proj-slide-wrap'>
-                <button>&lang;</button>
+                <SliderBtn sliderBtnTxt={'〈'} />
                 <div>
                     <Health />
                 </div>
-                <button>&rang;</button>
-
-                
+                <SliderBtn sliderBtnTxt={'〉'} />
             </div>
         )
     }
