f9d8327937d2b83d46e937c7f756ba658f7442d8
VIISilver <jparke71@hotmail.com>
2019-09-23 18:42:55 -0600

    Updated styling for the slider component.

--- src/components/projects/sliderComps/ProjectSlider.js
+++ src/components/projects/sliderComps/ProjectSlider.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Health from '../Health'
 import SliderBtn from './SliderBtn'
+import { Transition } from 'react-transition-group'
 import './ProjectSlider.css'
 
 export default class ProjectSlider extends Component {
@@ -8,9 +9,7 @@ export default class ProjectSlider extends Component {
         return (
             <div className='proj-slide-wrap'>
                 <SliderBtn sliderBtnTxt={'〈'} />
-                <div>
-                    <Health />
-                </div>
+                <Health />
                 <SliderBtn sliderBtnTxt={'〉'} />
             </div>
         )
