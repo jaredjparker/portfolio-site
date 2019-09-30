7509804b9809807498c0336f13dba670550b4216
VIISilver <jparke71@hotmail.com>
2019-09-29 23:29:19 -0600

    Added new position under experience and adjusted styling.

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 // import Overview from './Overview'
 import Contractor from './Contractor'
 import Impartner from './Impartner'
+import FEDImpartner from './FEDImpartner'
 import './Experience.css'
 
 export default class Experience extends Component {
@@ -9,6 +10,7 @@ export default class Experience extends Component {
         return (
             <div className='section-wrap experience-wrap'>
                 <h1>Experience</h1>
+                <FEDImpartner />
                 <Impartner />
                 <Contractor />
                 {/* <Overview /> */}
---
+++ src/components/experience/FEDImpartner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './FEDImpartner.css'
+
+export default function FEDImpartner() {
+    return (
+        <div className='experience-basic'>
+            <h2>Impartner Software</h2>
+            <p>Frontend Devleoper</p>
+            <p>September 2019 - Current</p>
+            <ul>
+                <li>Complete Frontend tasks needed for Customer Portals currently being implemented or existing portals requiring new frontend work. Using ASP.NET, Sass, and Knockout.js for the majority of tasks.</li>
+            </ul>
+        </div>
+    )
+}
