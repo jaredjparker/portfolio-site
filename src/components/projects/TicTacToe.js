ef56e49f91169d1ac1b8b32d1b55fd47c591cb77
VIISilver <jparke71@hotmail.com>
2019-10-08 17:40:32 -0600

    Corrected mobile styling issues.

--- src/components/projects/TicTacToe.js
+++ src/components/projects/TicTacToe.js
@@ -6,7 +6,7 @@ import tictactoeImage from '../../assets/tictactoe-project-image.jpg'
 
 export default function TicTacToe() {
 
-    let tictactoeListItemArr = ['Built for a presentation.', 'Help users learn React.js.', 'Player turns are maintained.', 'Validates upon win or draw.', 'Illegal move cannot be made.']
+    let tictactoeListItemArr = ['Built for a presentation.', 'Help users learn React.js.', 'Player turns are maintained.', 'Validates upon win or draw.', 'Illegal moves cannot be made.']
 
     return (
         <div className='specific-project-wrapper'>
